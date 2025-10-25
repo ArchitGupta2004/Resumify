98a702d2433c6d5601cc7d8c4f73115535ed134d
Archit Gupta <guptaarchit12345@gmail.com>
2025-10-25 16:10:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to Resumify 🧩</h1>
+      <p>Your personal resume builder using the MERN Stack!</p>
+    </div>
+  );
+}
+
+export default App;
